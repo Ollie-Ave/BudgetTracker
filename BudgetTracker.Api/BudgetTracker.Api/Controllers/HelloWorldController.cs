c15076ab5d65998dc7b9539533977358e75548d3
oliver@citruslime.com <oliver@citruslime.com>
2023-07-28 21:00:16 +0100

    Generated docs for HelloWorld controller

--- BudgetTracker.Api/BudgetTracker.Api/Controllers/HelloWorldController.cs
+++ BudgetTracker.Api/BudgetTracker.Api/Controllers/HelloWorldController.cs
@@ -1,18 +1,18 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Threading;
-
 namespace BudgetTracker.Api.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    
+    /// <summary>A controller for handling hello worlds.</summary>
+    /// <seealso cref="T:ControllerBase"/>
     [ApiController]
     [Route("api/[controller]")]
     public class HelloWorldController : ControllerBase
     {
+        /// <summary>(An Action that handles HTTP GET requests) gets the get.</summary>
+        /// <returns>An IActionResult.</returns>
         [HttpGet]
         public IActionResult Get()
         {
-            Thread.Sleep(1000);
-
             return this.Ok("Hello World!");
         }
     }
