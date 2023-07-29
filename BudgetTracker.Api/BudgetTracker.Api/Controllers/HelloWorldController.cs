3033663a36ab170985ba8f58bff68c46e1020472
oliver@citruslime.com <oliver@citruslime.com>
2023-07-29 13:01:46 +0100

    Removed docs for HelloWorldController

--- BudgetTracker.Api/BudgetTracker.Api/Controllers/HelloWorldController.cs
+++ BudgetTracker.Api/BudgetTracker.Api/Controllers/HelloWorldController.cs
@@ -2,14 +2,10 @@ namespace BudgetTracker.Api.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
     
-    /// <summary>A controller for handling hello worlds.</summary>
-    /// <seealso cref="T:ControllerBase"/>
     [ApiController]
     [Route("api/[controller]")]
     public class HelloWorldController : ControllerBase
     {
-        /// <summary>(An Action that handles HTTP GET requests) gets the get.</summary>
-        /// <returns>An IActionResult.</returns>
         [HttpGet]
         public IActionResult Get()
         {
