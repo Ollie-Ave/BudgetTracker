f8b8a93094cb2325e33710d4b50de24cbc5c3557
oliver@citruslime.com <oliver@citruslime.com>
2023-08-26 12:20:17 +0100

    Removed thread blocking from Spending chart

--- BudgetTracker/src/components/home/SpendingChart.vue
+++ BudgetTracker/src/components/home/SpendingChart.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 import { useAccountData } from '@/stores/accountData';
 import { useLoginStore } from '@/stores/loggedIn';
@@ -8,6 +8,8 @@ import { useApiRoutes } from '@/stores/apiRoutes';
 import handleApiError from '@/sharedLogic/handleApiErrors';
 import axios from 'axios';
 
+import Spinner from '@/components/shared/Spinner.vue';
+
 import { Line } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale } from 'chart.js';
 ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
@@ -15,8 +17,6 @@ ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, CategoryScal
 const accountDataStore = useAccountData();
 const loggedInStore = useLoginStore();
 const apiRoutesStore = useApiRoutes();
-
-const accountData = await accountDataStore.getAccountData(loggedInStore.apiKey);
 
 const colour_primary_300 = '#fb6a72';
 const colour_success = '#00bc4b';
@@ -72,49 +72,7 @@ async function getAccountDifferences(uid) {
     }
 }
 
-const ExpensesData = {
-    label: 'Outgoing',
-    backgroundColor: dot_colour,
-    borderColor: colour_primary_300,
-    data: await getAccountExpenses(accountData.uid)
-};
-
-const incomeData = {
-    label: 'Incoming',
-    backgroundColor: dot_colour,
-    borderColor: colour_success,
-    data: await getAccountIncome(accountData.uid)
-}
-
-const differenceData = {
-    label: 'Difference',
-    backgroundColor: dot_colour,
-    borderColor: colour_warning,
-    data: await getAccountDifferences(accountData.uid)
-}
-
-function getLabels(){
-    var labels = [];
-    var today = new Date();
-    for (var i = 0; i < 7; i++) {
-        // Generate the date for each label by subtracting the number of days from the current date
-        var date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
-        labels.push(date.toLocaleDateString('eb-gb', {weekday: 'long'}));
-    }
-
-    return labels.reverse();
-}
-
-
-
-const chartData = ref({
-    labels: getLabels(),
-    datasets: [
-        ExpensesData,
-        incomeData,
-        differenceData
-    ]
-});
+const chartData = ref(null);
 
 const chartOptions = ref({
     responsive: true,
@@ -126,6 +84,53 @@ const chartOptions = ref({
     }
 });
 
+onMounted( async () => {
+    const accountData = await accountDataStore.getAccountData(loggedInStore.apiKey);
+
+    const ExpensesData = {
+    label: 'Outgoing',
+    backgroundColor: dot_colour,
+    borderColor: colour_primary_300,
+    data: await getAccountExpenses(accountData.uid)
+    };
+
+    const incomeData = {
+        label: 'Incoming',
+        backgroundColor: dot_colour,
+        borderColor: colour_success,
+        data: await getAccountIncome(accountData.uid)
+    }
+
+    const differenceData = {
+        label: 'Difference',
+        backgroundColor: dot_colour,
+        borderColor: colour_warning,
+        data: await getAccountDifferences(accountData.uid)
+    }
+
+    chartData.value = {
+    labels: getLabels(),
+    datasets: [
+        ExpensesData,
+        incomeData,
+        differenceData
+    ]
+    };
+});
+
+
+function getLabels(){
+    let labels = [];
+    let today = new Date();
+    for (let i = 0; i < 7; i++) {
+        // Generate the date for each label by subtracting the number of days from the current date
+        let date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
+        labels.push(date.toLocaleDateString('eb-gb', {weekday: 'long'}));
+    }
+
+    return labels.reverse();
+}
+
 </script>
 
 <template>
@@ -134,7 +139,9 @@ const chartOptions = ref({
             <h2>Spending this week</h2>
             <hr>
             <div class="bar-chart">
-                <Line :data="chartData" :options="chartOptions" />
+                <Line v-if="chartData !== null" :data="chartData" :options="chartOptions" />
+
+                <Spinner  v-else class="center-spinner"/>
             </div>
 
         </div>
@@ -143,6 +150,13 @@ const chartOptions = ref({
 
 
 <style scoped>
+
+.center-spinner{
+    position: relative;
+    top: 50%;
+    left: 100%;
+    transform: translate(-50%, -50%);
+}
 
 .container{
     display: inline-block;
